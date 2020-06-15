2da85b0d02417229877c348e395668e0370c8f75
Samarth Ramesh <SAMARTHR1@OUTLOOK.COM>
2020-06-15 20:16:53 +0530

    modified:   attack.py Fixed while loop falling through
    TODO
    
    clean output
    transfer methods win and cont to adecide

--- fullrisk/attack.py
+++ fullrisk/attack.py
@@ -1,6 +1,17 @@
 import random
+import adecide
+from adecide import adecide
 class attack:
-			
+	def start(self,na,nd):
+		self.x=0
+		self.l=adecide()
+		self.m=attack()
+		if adecide.decide(self.l,na,nd)==1:
+			self.x=adecide.war(self.m,na,nd)
+		else:
+			pass
+		return self.x
+		
 	def war(self,na,nd):
 		self.a=0
 		self.m=0
@@ -14,7 +25,7 @@ class attack:
 			else:
 				self.k=0
 				self.numadie=attack.numadie(self.t,na)
-				self.numddie=attack.numadie(self.t,nd)
+				self.numddie=attack.numddie(self.t,nd)
 				self.aroll=attack.aroll(self.t,self.numadie)
 				self.droll=attack.droll(self.t,self.numddie)
 				self.out=attack.compare(self.t,self.aroll,self.droll)
@@ -28,6 +39,7 @@ class attack:
 			self.numadie=3
 		else:
 			self.numadie=na-1
+		#print("numadie= ",self.numadie)
 		return self.numadie
 	def numddie(self,nd):
 		if nd>1:
@@ -36,12 +48,14 @@ class attack:
 			self.numddie=1
 		else:
 			self.numddie=0
-	
+		#print("numddie= ",self.numddie)
+		return self.numddie
+		
 	def diecreate(self,num):
 		self.out=[]
 		for self.x in range(num):
 			self.out.append(0)
-		print("diecreated= ",self.out)
+		#print("diecreated= ",self.out)
 		return self.out
 	
 	def aroll(self,num):
@@ -50,6 +64,7 @@ class attack:
 		for self.x in range(num):
 			self.aroll[self.x]=random.randint(1,6)
 		self.aroll.sort(reverse=True)
+		print("aroll= ",self.aroll)
 		return self. aroll
 	
 	def droll(self,num):
@@ -58,23 +73,25 @@ class attack:
 		for self.x in range(num):
 			self.droll[self.x]=random.randint(1,6)
 		self.droll.sort(reverse=True)
+		print("droll= ",self.droll)
 		return self.droll
 	
 	def compare(self,aroll,droll):
 		self.out=[0,0]
-		self.x=0
-		for x in droll:
+		for self.x in range(len(droll)):
 			if aroll[self.x] > droll[self.x]:
-				self.out[0]=self.out[0]-1
-			else:
 				self.out[1]=self.out[1]-1
+				print("defense lost 1")
+			else:
+				self.out[0]=self.out[0]-1
+				print("attack lost1")
 			self.x=self.x+1
 		print("out= ", self.out)
 		return self.out
 	
 	def aresult(self,out,na):
 		na=na+out[0]
-		#print(na)
+		print("aresult= ",na)
 		return na
 	def dresult(self,out,nd):
 		nd=nd+out[1]
@@ -90,10 +107,10 @@ class attack:
 	
 	def  cont(self,na,nd):
 		a=attack()
-		if ((na-nd>5) or (nd<2 and na>4)or(attack.decide(a,na,nd)>0)):
-			self.x=1
-		else:
+		if ((na-nd>5) or (nd<2 and na>4)):
 			self.x=0
+		else:
+			self.x=1
 		return self.x
 	
 	
@@ -101,7 +118,7 @@ def main():
 	#na=int(input("na"))
 	#nd=int(input("nd")) 
 	na=1000
-	nd=2
+	nd=20
 	a=attack()
 	print(attack.war(a,na,nd))
 main()
